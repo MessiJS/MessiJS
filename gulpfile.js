35578abb988724196d58d78167d37780c7c7fcf7
Kevin Gustavson <kdgustavson@gmail.com>
2017-01-31 08:06:16 -0600

    Modernize gulfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,121 +1,99 @@
-var pkg = require('./package.json');
+const pkg = require('./package.json');
 
-var gulp        = require('gulp');
-var concat      = require('gulp-concat');
-var coveralls   = require('gulp-coveralls');
-var cssnano     = require('gulp-cssnano');
-var del         = require('del');
-var eventStream = require('event-stream');
-var insert      = require('gulp-insert');
-var jshint      = require('gulp-jshint');
-var Karma       = require('karma').Server;
-var notify      = require('gulp-notify');
-var rename      = require('gulp-rename');
-var sourcemaps  = require('gulp-sourcemaps');
-var uglify      = require('gulp-uglify');
-var zip         = require('gulp-zip');
-var gutil       = require('gulp-util');
+const gulp        = require('gulp');
+const concat      = require('gulp-concat');
+const coveralls   = require('gulp-coveralls');
+const cssnano     = require('gulp-cssnano');
+const del         = require('del');
+const eventStream = require('event-stream');
+const insert      = require('gulp-insert');
+const jshint      = require('gulp-jshint');
+const Karma       = require('karma').Server;
+const notify      = require('gulp-notify');
+const rename      = require('gulp-rename');
+const sourcemaps  = require('gulp-sourcemaps');
+const uglify      = require('gulp-uglify');
+const zip         = require('gulp-zip');
+const gutil       = require('gulp-util');
 
-var sources = ['src/*.js', 'test/*Spec.js'];
+const sources = ['src/*.js', 'test/*Spec.js'];
 
-var banner = [
+const banner = [
     '/**!',
-    ' * ' + pkg.name + ' - ' + pkg.description,
-    ' * @version ' + pkg.version,
-    ' * @link ' + pkg.homepage,
-    ' * @license ' + pkg.license,
+    ` * ${pkg.name} - ${pkg.description}`,
+    ` * @version ${pkg.version}`,
+    ` * @link ${pkg.homepage}`,
+    ` * @license ${pkg.license}`,
     ' * @copyright Copyright 2012-13 Marcos Esperón',
     ' * @copyright Copyright 2014-17 Kevin Gustavson',
     ' */',
     ''].join('\n');
 
-gulp.task('clean', function(done) {
-    return del([ 'dist', 'coverage' ], done);
-});
+gulp.task('clean', done => del([ 'dist', 'coverage' ], done));
 
-gulp.task('lint', function() {
-    return gulp.src(sources)
-        .pipe(jshint())
-        .pipe(jshint.reporter('default'));
-});
+gulp.task('lint', () => gulp.src(sources)
+    .pipe(jshint())
+    .pipe(jshint.reporter('default')));
 
-gulp.task('create-dist', ['clean'], function() {
+gulp.task('create-dist', ['clean'], () => {
     gutil.File({ path: 'dist/' });
     return gulp.src(['README.md'])
         .pipe(gulp.dest('dist'));
 });
 
-gulp.task('combine', ['create-dist'], function() {
-    return eventStream.merge(
-        gulp.src(['src/*.css'])
-            .pipe(concat('messi.css'))
-            .pipe(gulp.dest('dist')),
-        gulp.src(['src/*.js'])
-            .pipe(concat('messi.js'))
-            .pipe(gulp.dest('dist'))
-    );
-});
+gulp.task('combine', ['create-dist'], () => eventStream.merge(
+    gulp.src(['src/*.css'])
+        .pipe(concat('messi.css'))
+        .pipe(gulp.dest('dist')),
+    gulp.src(['src/*.js'])
+        .pipe(concat('messi.js'))
+        .pipe(gulp.dest('dist'))
+));
 
-gulp.task('compress', ['create-dist'], function() {
-    return eventStream.merge(
-        gulp.src(['src/*.js'])
-            .pipe(sourcemaps.init())
-                .pipe(concat('messi.min.js'))
-                .pipe(uglify())
-            .pipe(sourcemaps.write('.'))
-            .pipe(gulp.dest('dist')),
+gulp.task('compress', ['create-dist'], () => eventStream.merge(
+    gulp.src(['src/*.js'])
+        .pipe(sourcemaps.init())
+            .pipe(concat('messi.min.js'))
+            .pipe(uglify())
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('dist')),
 
-        gulp.src(['src/*.css'])
-            //.pipe(sourcemaps.init())
-                .pipe(concat('messi.min.css'))
-                .pipe(cssnano())
-            //.pipe(sourcemaps.write('.'))
-            .pipe(gulp.dest('dist'))
-    );
-});
+    gulp.src(['src/*.css'])
+        //.pipe(sourcemaps.init())
+            .pipe(concat('messi.min.css'))
+            .pipe(cssnano())
+        //.pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('dist'))
+));
 
-gulp.task('add-banner', ['combine', 'compress'], function() {
-    return eventStream.merge(
-        gulp.src(['dist/messi.js', 'dist/messi*.css'])
-            .pipe(insert.prepend(banner))
-            .pipe(gulp.dest('dist')),
-        gulp.src('dist/messi.min.js')
-            .pipe(insert.wrap(banner, '\n//# sourceMappingURL=messi.min.js.map'))
-            .pipe(gulp.dest('dist'))
-    );
-});
+gulp.task('add-banner', ['combine', 'compress'], () => eventStream.merge(
+    gulp.src(['dist/messi.js', 'dist/messi*.css'])
+        .pipe(insert.prepend(banner))
+        .pipe(gulp.dest('dist')),
+    gulp.src('dist/messi.min.js')
+        .pipe(insert.wrap(banner, '\n//# sourceMappingURL=messi.min.js.map'))
+        .pipe(gulp.dest('dist'))
+));
 
-gulp.task('test', ['lint'], function(done) {
-    return Karma.start({
-        configFile: __dirname + '/karma.conf.js',
-        singleRun: true
-    }, done);
-});
+gulp.task('test', ['lint'], done => Karma.start({
+    configFile: `${__dirname}/karma.conf.js`,
+    singleRun: true
+}, done));
 
-gulp.task('codecoverage', ['test'], function(done) {
-    return gulp.src('coverage/**/lcov.info')
-        .pipe(coveralls(done));
-});
+gulp.task('codecoverage', ['test'], done => gulp.src('coverage/**/lcov.info')
+    .pipe(coveralls(done)));
 
-gulp.task('zip', ['add-banner'], function() {
-    return gulp.src('dist/*')
-        .pipe(zip('MessiJS.zip'))
-        .pipe(gulp.dest('dist'));
-});
+gulp.task('zip', ['add-banner'], () => gulp.src('dist/*')
+    .pipe(zip('MessiJS.zip'))
+    .pipe(gulp.dest('dist')));
 
-gulp.task('watch', function() {
-    return gulp.watch(sources, ['default']);
-});
+gulp.task('watch', () => gulp.watch(sources, ['default']));
 
-gulp.task('notify:test', ['test'], function() {
-    return gulp.src('./gulpfile.js')
-        .pipe(notify({ message: 'All done, master!' }));
-});
+gulp.task('notify:test', ['test'], () => gulp.src('./gulpfile.js')
+    .pipe(notify({ message: 'All done, master!' })));
 
-gulp.task('notify:zip', ['zip'], function() {
-    return gulp.src('./gulpfile.js')
-        .pipe(notify({ message: 'Zip file has been created.' }));
-});
+gulp.task('notify:zip', ['zip'], () => gulp.src('./gulpfile.js')
+    .pipe(notify({ message: 'Zip file has been created.' })));
 
 gulp.task('default', ['zip', 'test']);
 gulp.task('travis-test', ['codecoverage']);
